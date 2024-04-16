3f260cbba11b492c3d4537a0913badccfc5c4be6
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2024-04-16 10:59:17 +0000

    solution added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react/cjs/react.production.min";
 
 const App = () => {
+  const [state,setState]=useState("");
+
+  const coppy =(event)=>{
+    setState(event.target.value);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input onKeyUp={coppy}/>
+        <p>{state}</p>
     </div>
   )
 }
